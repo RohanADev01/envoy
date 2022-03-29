d275648a69f6c308820ecc7189cb209d87653f6e
RohanA <rohanagarwal0192@gmail.com>
2022-03-30 00:14:36 +1100

    basic client slide routing implemented for landing page using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import Home from './Components/LandingPage/Home';
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./Components/LandingPage/Home";
 import DashboardContents from "./Components/Dashboard/Dashboard";
-import SignUp from './Components/LandingPage/Signup';
-import Login from './Components/LandingPage/Login';
-  
+import SignUp from "./Components/LandingPage/Signup";
+import Login from "./Components/LandingPage/Login";
+
 function App() {
     return (
-        <div>
-            {/* <DashboardContents/> */}
-            {/* <Home/> */}
-            <SignUp/>
-            {/* <Login/> */}
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}>
+                    <Route path="signup" element={<SignUp />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="*" element={<h1>Error 404. Page not found!</h1>} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
