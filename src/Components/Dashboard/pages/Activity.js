d83401690e684cfaf16ffc210dce5a6bc74854e7
Peter Louka <p.louka@student.unsw.edu.au>
2022-04-02 13:41:40 +1100

    fix merge

--- src/Components/Dashboard/pages/Activity.js
+++ src/Components/Dashboard/pages/Activity.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import { Typography } from '@mui/material';
-import { useAuthDataContext } from '../../Landing/userAuth'
-
-export const Activity = () => {
-  const auth = useAuthDataContext();
-
-  return (
-    <Typography component="h1" fontSize="1.8rem" fontFamily="Montserrat">Welcome {auth.email}</Typography>
-  )
-}
-=======
 import React from "react";
 import { Typography } from "@mui/material";
 import { useAuthDataContext } from "../../Landing/userAuth";
@@ -24,4 +11,3 @@ export const Activity = () => {
         </Typography>
     );
 };
->>>>>>> main
