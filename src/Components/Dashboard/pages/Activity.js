0772512f7c098e7a54fdca370bf9cfb6d0f667c1
RohanA <rohanagarwal0192@gmail.com>
2022-04-02 13:35:46 +1100

    formatting all files and removing unnecessary comments

--- src/Components/Dashboard/pages/Activity.js
+++ src/Components/Dashboard/pages/Activity.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import { Typography } from '@mui/material';
-import { useAuthDataContext } from '../../Landing/userAuth'
+import React from "react";
+import { Typography } from "@mui/material";
+import { useAuthDataContext } from "../../Landing/userAuth";
 
 export const Activity = () => {
-  const auth = useAuthDataContext();
+    const auth = useAuthDataContext();
 
-  return (
-    <Typography component="h1" fontSize="1.8rem" fontFamily="Montserrat">Welcome {auth.email}</Typography>
-  )
-}
+    return (
+        <Typography component="h1" fontSize="1.8rem" fontFamily="Montserrat">
+            Welcome {auth.email}
+        </Typography>
+    );
+};
