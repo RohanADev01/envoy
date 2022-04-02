fc878ee05627c77b36f47fdd625571cef954a392
RohanA <rohanagarwal0192@gmail.com>
2022-04-02 13:22:05 +1100

    dashboard pages linked to sidebar menu

---
+++ src/Components/Dashboard/pages/MyInvoices.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Typography } from '@mui/material';
+
+export const MyInvoices = () => {
+  return (
+    <Typography component="h1" fontSize="1.8rem" fontFamily="Montserrat">My Invoices</Typography>
+  )
+}
