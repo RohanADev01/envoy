2d1291529dd84ec8bfd15a38187bf468242d107c
RohanA <rohanagarwal0192@gmail.com>
2022-04-09 01:37:16 +1000

    experimenting with styles, need to clear up

---
+++ src/Components/Dashboard/styles.js
@@ -0,0 +1,22 @@
+// export const success = { color: 'white', backgroundColor: "#3CD4A0" }
+export const success = { color: 'white', backgroundColor: "#2A9D8F" }
+export const warning = { color: 'white', backgroundColor: "#FFC260" }
+export const secondary = { color: 'white', backgroundColor: "#FF5C93" }
+
+const fonts = { color: "#264653", family: "Roboto, Helvetica, Arial" }
+
+// Widget
+export const tableContent = { color: fonts.color, fontFamily: fonts.family, fontSize: "0.95rem", fontWeight: "500", lineHeight: "1.5rem", padding: "16px 16px 16px 24px" }
+export const cardHeader = { color: "#7b7b7b", fontFamily: fonts.family, fontSize: "1.285rem", fontWeight: "400", lineHeight: "1.334", padding: "16px 16px 16px 0px" }
+// export const pageTitle = { color: "#B9B9B9", fontFamily: fonts.family, fontSize: "2.4rem", fontWeight: "400", marginBottom: "1.75rem" }
+// export const pageTitle = { color: "#ADADAD", fontFamily: fonts.family, fontSize: "2.4rem", fontWeight: "400", marginBottom: "1.75rem" }
+// export const pageTitle = { color: "#264653", fontFamily: fonts.family, fontSize: "2.4rem", fontWeight: "400", marginBottom: "1.75rem" }
+export const pageTitle = { color: "#888888", fontFamily: fonts.family, fontSize: "2.4rem", fontWeight: "400", marginBottom: "1.75rem" }
+
+// Card 1 Monthly Earnings
+export const fullHeightBody = { display: "flex", flexGrow: 1, flexDirection: "column", justifyContent: "space-between" }
+export const statsBig = { color: fonts.color, fontFamily: fonts.family, fontSize: "28px", fontWeight: "300", }
+export const statsSmall = { color: fonts.color, fontFamily: fonts.family, fontSize: "21px", fontWeight: "300", }
+export const statsSmallHeader = { color: "rgb(110, 110, 110)", fontFamily: fonts.family, fontSize: "14px" }
+export const card = { minHeight: "100%", display: "flex", flexDirection: "column" }
+export const visitsNumberContainer = { display: "flex", alignItems: "center", flexGrow: 1, padding: "24px 0 24px 0" }
