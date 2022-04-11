1c1e177e6a475a6eb640439914ebcc36f0088060
RohanA <rohanagarwal0192@gmail.com>
2022-04-11 18:42:26 +1000

    get values from text fields for new first/last name and update state

--- src/Components/Dashboard/Pages/Profile.js
+++ src/Components/Dashboard/Pages/Profile.js
@@ -5,50 +5,79 @@ import ProfileTop from '../ProfileTop'
 import { userDetails } from '../UserDetails'
 
 const Profile = (props) => {
-    const [localColor, setLocalColor] = useState(userDetails.hex_colour ? userDetails.hex_colour : "#1ABC9C");
+    const [localColor, setLocalColor] = useState(props.userProfileState['profileColor'] ? props.userProfileState['profileColor'] : "#1ABC9C");
+
+    const handleFirstnameChange = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const newFirst = data.get("newFirst")
+        localStorage.setItem("firstname", newFirst)
+        props.userProfileState['setProfileFirstName'](newFirst)
+    }
+
+    const handleLastnameChange = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const newLast = data.get("newLast")
+        localStorage.setItem("lastname", newLast)
+        props.userProfileState['setProfileLastName'](newLast)
+    }
 
     const handleAvatarColorChange = () => {
         localStorage.setItem("hex_color", localColor)
-        props.profileColorState['setProfileColor'](localColor)
+        props.userProfileState['setProfileColor'](localColor)
     }
 
     return (
-        <Card>
-            <ProfileTop profileColorState={props.profileColorState} />
+        <Card sx={{ paddingBottom: 2 }}>
+            <ProfileTop userProfileState={props.userProfileState} />
             <Grid container display="flex" justifyContent="center" alignItems="flex-start" spacing={4}>
                 <Grid item xs={6}>
                     <Grid container justifyContent="center" alignItems='center' flexDirection='column' spacing={3}>
                         <Grid item xs={6}>
                             <Typography variant='h5'>
-                                {`Firstname: ${userDetails.firstname}`}
+                                {`Current name: ${props.userProfileState['profileFirstName']} ${props.userProfileState['profileLastName']}`}
                             </Typography>
                         </Grid>
-                        <Button>Edit firstname</Button>
-                        <Grid item xs={6}>
-                            <Typography variant='h5'>
-                                {`Firstname: ${userDetails.lastname}`}
-                            </Typography>
-                            <TextField>Change username</TextField>
-                        </Grid>
-                        <Button>Edit lastname</Button>
+
+
+                        <Box component='form' onSubmit={handleFirstnameChange} sx={{ margin: 2 }}>
+                            <Grid item>
+                                <TextField required label="enter new firstname" name="newFirst" id="newFirst" variant="filled">Change username</TextField>
+                            </Grid>
+                            <Button type='submit' variant='contained'>Change firstname</Button>
+                        </Box>
+
+                        <Box component='form' onSubmit={handleLastnameChange} sx={{ margin: 2 }}>
+                            <Grid item>
+                                <TextField required label="enter new lastname" name="newLast" id="newLast" variant="filled">Change username</TextField>
+                            </Grid>
+                            <Button type='submit' variant='contained'>Change lastname</Button>
+                        </Box>
                     </Grid>
                 </Grid>
                 <Grid item xs={6}>
                     <Grid container justifyContent="center" alignItems='center' flexDirection="column" spacing={3}>
                         <Grid item xs={6}>
                             <Typography variant='h5'>
-                                {`Profile Colour: ${props.profileColorState['profileColor']}`}
+                                {`Current Profile Colour: ${props.userProfileState['profileColor']}`}
                             </Typography>
+                        </Grid>
+                        <Grid item xs={6}>
                             <HexColorPicker color={localColor} onChange={setLocalColor} />
+                        </Grid>
+                        <Grid item xs={6}>
                             <Typography variant='h5'>
-                                {`Choosing ${localColor}`}
+                                {`Color Picker value: ${localColor}`}
                             </Typography>
+                        </Grid>
+                        <Grid item xs={6}>
                             <Button onClick={handleAvatarColorChange}>set logo color</Button>
                         </Grid>
                     </Grid>
                 </Grid>
             </Grid>
-        </Card>
+        </Card >
     )
 }
 
