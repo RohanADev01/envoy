3e256fc2d8fea5180c2afd4218d44803b3928b11
RohanA <rohanagarwal0192@gmail.com>
2022-04-12 19:25:41 +1000

    reset password, alerts, and fetch API calls

--- src/Components/Dashboard/Pages/Profile.js
+++ src/Components/Dashboard/Pages/Profile.js
@@ -1,8 +1,12 @@
 import { Card, Box, Grid, Typography, TextField, Button } from '@mui/material'
 import React, { useState } from 'react'
 import { HexColorPicker } from 'react-colorful'
+import { trackPromise } from 'react-promise-tracker'
 import { toast } from 'react-toastify'
+import { backend_base_url } from '../../../Constants'
 import ProfileTop from '../ProfileTop'
+import axios from 'axios'
+import { FailAlert } from '../../Landing/Constants'
 
 const Profile = (props) => {
     const [localColor, setLocalColor] = useState(props.userProfileState['profileColor'] ? props.userProfileState['profileColor'] : "#1ABC9C");
@@ -12,8 +16,27 @@ const Profile = (props) => {
         const data = new FormData(event.currentTarget);
         const newFirst = data.get("newFirst")
         localStorage.setItem("firstname", newFirst)
+
+        // send data to server
+        const update_firstname_url = backend_base_url + 'user/update/firstname'
+        const body = { "email": props.userProfileState['profileEmail'], "new_firstname": newFirst }
+        console.log("hello")
+        trackPromise(
+            axios({
+                method: 'POST',
+                url: update_firstname_url,
+                data: body,
+            })
+                .then((data) => {
+                    console.log(data)
+                    toast(`Name updated to ${newFirst} ${props.userProfileState['profileLastName']}`)
+                })
+                .catch((error) => {
+                    toast(error)
+                })
+        )
+        console.log("going?")
         props.userProfileState['setProfileFirstName'](newFirst)
-        toast(`Name updated to ${newFirst} ${props.userProfileState['profileLastName']}`)
     }
 
     const handleLastnameChange = (event) => {
@@ -22,7 +45,66 @@ const Profile = (props) => {
         const newLast = data.get("newLast")
         localStorage.setItem("lastname", newLast)
         props.userProfileState['setProfileLastName'](newLast)
-        toast(`Name updated to ${props.userProfileState['profileFirstName']} ${newLast}`)
+
+        // send data to server
+        const update_lastname_url = backend_base_url + 'user/update/lastname'
+        const body = { "email": props.userProfileState['profileEmail'], "new_lastname": newLast }
+
+        trackPromise(
+            axios({
+                method: 'POST',
+                url: update_lastname_url,
+                data: body,
+            })
+                .then((data) => {
+                    toast(`Name updated to ${props.userProfileState['profileFirstName']} ${newLast}`)
+                })
+                .catch((error) => {
+                    toast(error)
+                })
+        )
+    }
+
+    const [alertFail, setFailAlert] = useState(false)
+    const [alertContent, setAlertContent] = useState('')
+
+    const handleResetPassword = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const password = data.get("oldpass")
+        const new_password = data.get("newpass")
+
+        // send data to server
+        const reset_password_url = backend_base_url + 'user/update/password'
+        const body = { "email": props.userProfileState['profileEmail'], password, new_password }
+
+        trackPromise(
+            axios({
+                method: 'POST',
+                url: reset_password_url,
+                data: body,
+            })
+                .then((data) => {
+                    let msg = data.data.msg
+                    setAlertContent(msg)
+
+                    if (msg === "Password successfully updated") {
+                        toast(msg)
+                    } else {
+                        setFailAlert(true)
+                    }
+                })
+                .catch((error) => {
+                    setAlertContent(
+                        'An unknown error occured, please try again another time.'
+                    )
+                    setFailAlert(true)
+                })
+        )
+    }
+
+    const resetAlerts = (event) => {
+        setFailAlert(false)
     }
 
     const handleAvatarColorChange = () => {
@@ -43,7 +125,6 @@ const Profile = (props) => {
                             </Typography>
                         </Grid>
 
-
                         <Box component='form' onSubmit={handleFirstnameChange} sx={{ margin: 2 }}>
                             <Grid item>
                                 <TextField required label="enter new firstname" name="newFirst" id="newFirst" variant="filled">Change username</TextField>
@@ -57,6 +138,26 @@ const Profile = (props) => {
                             </Grid>
                             <Button type='submit' variant='contained'>Change lastname</Button>
                         </Box>
+
+                        <Grid item xs={6}>
+                            <Typography variant='h5'>
+                                Change Password
+                            </Typography>
+                        </Grid>
+
+                        <Box component='form' onChange={resetAlerts} onSubmit={handleResetPassword} sx={{ margin: 2 }}>
+                            <Grid item>
+                                <TextField required type="password" label="enter old password" name="oldpass" id="oldpass" variant="filled">Old Password</TextField>
+                            </Grid>
+                            <Grid item>
+                                <TextField required type="password" label="enter new password" name="newpass" id="newpass" variant="filled">New password</TextField>
+                            </Grid>
+                            <Button type='submit' variant='contained'>Reset Password</Button>
+                        </Box>
+                        <FailAlert
+                            alertFail={alertFail}
+                            alertContent={alertContent}
+                        />
                     </Grid>
                 </Grid>
                 <Grid item xs={6}>
@@ -75,7 +176,7 @@ const Profile = (props) => {
                             </Typography>
                         </Grid>
                         <Grid item xs={6}>
-                            <Button onClick={handleAvatarColorChange}>set logo color</Button>
+                            <Button onClick={handleAvatarColorChange}>set avatar color</Button>
                         </Grid>
                     </Grid>
                 </Grid>
